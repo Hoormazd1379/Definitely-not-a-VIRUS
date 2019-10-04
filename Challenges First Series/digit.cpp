3621fc0ccf23699f3a6950bd7d950511a34d642e
Hoormazd Pirayeshfar <42213001+Hoormazd1379@users.noreply.github.com>
2019-10-04 23:07:05 +0200

    Update digit.cpp
    updated comments

--- Challenges First Series/digit.cpp
+++ Challenges First Series/digit.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+//a function that reverses an integer number
 int reversDigits(int num) 
 { 
     int rev_num = 0; 
@@ -14,8 +15,16 @@ int reversDigits(int num)
 
 int main()
 {
+
     int a, b = 0, c;
+	//a is the input number
+	//b is the output number
+	//c is the remainder counter
+	
+	//getting the input
     std::cin>>a;
+	
+	//checking for special cases
     if (a==0)
     {
         std::cout<<0;
@@ -24,6 +33,9 @@ int main()
     {
         std::cout<<1;
     }
+	
+	//program starts to count down from 9 to 2 and checks if the number is devisible by those digits. 
+	//if yes, it will devide it as much as possible before moving on to the next digit.
     else
     {
         for (int i = 9; i > 1; i--)
@@ -31,6 +43,8 @@ int main()
             c = a %i;
             while (c==0)
             {
+				//b will be the biggest number, which the product of it's digits are equal to the input number.
+				//therefore, it should be reversed
                 b = (b*10)+i;
                 a = a/i;
                 c = a%i;
@@ -43,6 +57,10 @@ int main()
         }
         else
         {
+			
+			// reverse integer 
+			//now we use the function
+			
               //  int reversedNumber = 0, r;
               //  while(b != 0)
               //  {
