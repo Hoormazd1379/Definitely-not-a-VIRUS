a3ae58e210c50edd3ade1278a4f4aeddc26106cc
Valsecchi Michelangelo <valsem@usi.ch>
2019-11-06 19:19:33 +0100

    Isograms challenge
    It shoudl word but the site says it doesn't, can you check it?

---
+++ isograms.py
@@ -0,0 +1,26 @@
+n = int(input())
+words = []
+results = []
+for i in range(n):
+    words.append(input())
+
+#print(words)
+
+def check(word):
+    letters = list(word)
+    letters.sort()
+    single_letter = list(dict.fromkeys(letters))
+    num = []
+    
+    for l in single_letter:
+        num.append(letters.count(l))
+        
+    if ((sum(num)/len(num))%num[0] == 0):
+        return "YES"
+    else:
+        return "NO"
+
+
+    
+for i in range(n):
+    print("Test #"+str(i+1)+": "+check(words[i]))
